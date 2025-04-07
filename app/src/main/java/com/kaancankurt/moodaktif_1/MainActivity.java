0682f0a850af26148fdde3451cc25651e1c3e730
[Kaan Can Kurt] <kaancan368368@gmail.com>
2025-04-08 01:01:35 +0300

    4. değişiklik bottomNavbar eklendi

--- app/src/main/java/com/kaancankurt/moodaktif_1/MainActivity.java
+++ app/src/main/java/com/kaancankurt/moodaktif_1/MainActivity.java
@@ -1,14 +1,44 @@
 package com.kaancankurt.moodaktif_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.kaancankurt.moodaktif_1.databinding.ActivityBottomnavbarBinding;
+
 public class MainActivity extends AppCompatActivity {
-    // değişiklik yapıldı
+    ActivityBottomnavbarBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        binding = ActivityBottomnavbarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
